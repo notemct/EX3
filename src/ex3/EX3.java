63ea0fb99839f61b85e9571cce0bbdc59f4c6078
notemct <note_comgear_x@hotmail.co.th>
2014-05-30 14:17:03 +0700

    EX1

---
+++ src/ex3/EX3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ex3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class EX3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println(10.0/3.0+5.0*2.0);
+         System.out.println(10-3%5*2);
+         System.out.println(10/3+5/2);
+         System.out.println(13%5/2);
+         System.out.println((10+3/2)*3);
+         System.out.println(5.0%3.0);
+             
+    }
+    
+}
